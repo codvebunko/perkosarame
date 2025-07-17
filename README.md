e01d4f2b9ee7b324d1ddbdf3199dca35a3321602
codvebunko <codvebunko@gmail.com>
2025-07-17 19:59:38 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# perkosarame
-perkosarame
+# [perkosarame](https://perkosarame.pages.dev)
---
+++ src/utils/site.js
@@ -0,0 +1,9 @@
+export const nama = 'Perkosa Rame';
+export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Smp Mama Viral Sma Cindo Tante China';
+export const url = 'https://perkosarame.pages.dev';
+export const terbit = '2024-12-17T00:01:03Z';
+
+export const google = '1oxRBVcVAv9h4Je5juLwNgFigVR2AiMB5_R_ZZfLHmI';
+export const yandex = '93ef9976e97590d7'
+
+export const index = 'e240fb06-49e0-46c8-995d-de181cbec412';
